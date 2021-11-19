44b12347af6c97c80e4d4db774bf73bdcc53cd22
MasterDoramu <qwerdc1234567890@gmail.com>
2021-11-20 00:01:01 +0300

    qr

---
+++ src/components/QrCodesPage/QrCodesPage.vue
@@ -0,0 +1,427 @@
+<template>
+  <div class="d-flex justify-content-center">
+    <div class="w-100 d-flex">
+      <b-container class="d-flex h-75">
+        <b-col cols="5" class="border m-4 p-4">
+          <div class="border-bottom">
+            <h4><b>№ 647019639200</b></h4>
+          </div>
+          <b-row class="border-bottom text-center">
+            <div class="p-3">
+              <b-row class="text-muted">
+                <span>Дата отправки</span>
+              </b-row>
+              <b-row>
+                <span>26 октября 2021 г.</span>
+              </b-row>
+            </div>
+            <b-col></b-col>
+            <div class=" p-3 text-right">
+              <h1><b>14:45</b></h1>
+            </div>
+          </b-row>
+          <b-row>
+            <b-row class="w-100">
+              <b-col cols="3">
+                <span>Откуда</span>
+              </b-col>
+              <b-col cols="4" class="border-bottom text-center">
+              </b-col>
+              <b-col class="text-right">
+                <span>Пункт отправления</span>
+              </b-col>
+            </b-row>
+          </b-row>
+          <b-row>
+            <b-row class="w-100">
+              <b-col cols="3">
+                <span>Куда</span>
+              </b-col>
+              <b-col cols="4" class="border-bottom text-center">
+              </b-col>
+              <b-col class="text-right">
+                <span>Пункт прибывания</span>
+              </b-col>
+            </b-row>
+          </b-row>
+          <b-row>
+            <b-row class="w-100">
+              <b-col cols="3">
+                <span>Судно</span>
+              </b-col>
+              <b-col cols="4" class="border-bottom text-center">
+              </b-col>
+              <b-col class="text-right">
+                <span>Метеор «Быстрый»</span>
+              </b-col>
+            </b-row>
+          </b-row>
+          <b-row>
+            <b-row class="w-100">
+              <b-col cols="3">
+                <span>Время в пути</span>
+              </b-col>
+              <b-col cols="4" class="border-bottom text-center">
+              </b-col>
+              <b-col class="text-right">
+                <span>2 часа</span>
+              </b-col>
+            </b-row>
+          </b-row>
+          <b-row>
+            <b-row class="w-100">
+              <b-col cols="3">
+                <span>Код бронирования</span>
+              </b-col>
+              <b-col cols="4" class="border-bottom text-center">
+              </b-col>
+              <b-col class="text-right">
+                <span>GADLVO</span>
+              </b-col>
+            </b-row>
+          </b-row>
+          <b-row>
+            <b-row class="w-100">
+              <b-col cols="3">
+                <span>Взрослый</span>
+              </b-col>
+              <b-col cols="4" class="border-bottom text-center">
+              </b-col>
+              <b-col class="text-right">
+                <span><b>1800</b></span>
+              </b-col>
+            </b-row>
+          </b-row>
+          <b-row class="d-flex justify-content-center pt-3">
+            <b-button variant="outline-secondary">Отменить поездку</b-button>
+          </b-row>
+        </b-col>
+        <b-col>
+          <div cols="5" class="border m-4 p-5">
+            <div class="border-bottom d-flex flex-column align-items-center">
+              <div>
+                <img src="../../assets/image1.png" alt="qrCode">
+              </div>
+              <div>
+                <h4><b>№ 647019639200</b></h4>
+              </div>
+            </div>
+            <b-row class="border-bottom text-center">
+              <div class="p-3">
+                <b-row class="text-muted">
+                  <span>Дата отправки</span>
+                </b-row>
+                <b-row>
+                  <span>26 октября 2021 г.</span>
+                </b-row>
+              </div>
+              <b-col></b-col>
+              <div class=" p-3 text-right">
+                <h1><b>14:45</b></h1>
+              </div>
+            </b-row>
+            <b-row>
+              <b-row class="w-100">
+                <b-col cols="3">
+                  <span>Откуда</span>
+                </b-col>
+                <b-col cols="4" class="border-bottom text-center">
+                </b-col>
+                <b-col class="text-right">
+                  <span>Пункт отправления</span>
+                </b-col>
+              </b-row>
+            </b-row>
+            <b-row>
+              <b-row class="w-100">
+                <b-col cols="3">
+                  <span>Куда</span>
+                </b-col>
+                <b-col cols="4" class="border-bottom text-center">
+                  <span></span>
+                </b-col>
+                <b-col class="text-right">
+                  <span>Пункт прибывания</span>
+                </b-col>
+              </b-row>
+            </b-row>
+            <b-row>
+              <b-row class="w-100">
+                <b-col cols="3">
+                  <span>Судно</span>
+                </b-col>
+                <b-col cols="4" class="border-bottom text-center">
+                </b-col>
+                <b-col class="text-right">
+                  <span>Метеор «Быстрый»</span>
+                </b-col>
+              </b-row>
+            </b-row>
+            <b-row>
+              <b-row class="w-100">
+                <b-col cols="3">
+                  <span>Время в пути</span>
+                </b-col>
+                <b-col cols="4" class="border-bottom text-center">
+                  <span></span>
+                </b-col>
+                <b-col class="text-right">
+                  <span>2 часа</span>
+                </b-col>
+              </b-row>
+            </b-row>
+            <b-row>
+              <b-row class="w-100">
+                <b-col cols="3">
+                  <span>Код бронирования</span>
+                </b-col>
+                <b-col cols="4" class="border-bottom text-center">
+                </b-col>
+                <b-col class="text-right">
+                  <span>GADLVO</span>
+                </b-col>
+              </b-row>
+            </b-row>
+            <b-row>
+              <b-row class="w-100">
+                <b-col cols="3">
+                  <span>Взрослый</span>
+                </b-col>
+                <b-col cols="4" class="border-bottom text-center">
+                  <span></span>
+                </b-col>
+                <b-col class="text-right">
+                  <span><b>3600</b></span>
+                </b-col>
+              </b-row>
+            </b-row>
+            <b-row class="d-flex justify-content-center pt-3">
+              <b-button variant="outline-secondary">Отменить поездку</b-button>
+            </b-row>
+          </div>
+        </b-col>
+      </b-container>
+      <b-container class="d-flex h-75">
+        <b-col cols="5" class="border m-4 p-4">
+          <div class="border-bottom">
+            <h4><b>№ 647019639200</b></h4>
+          </div>
+          <b-row class="border-bottom text-center">
+            <div class="p-3">
+              <b-row class="text-muted">
+                <span>Дата отправки</span>
+              </b-row>
+              <b-row>
+                <span>26 октября 2021 г.</span>
+              </b-row>
+            </div>
+            <b-col></b-col>
+            <div class=" p-3 text-right">
+              <h1><b>14:45</b></h1>
+            </div>
+          </b-row>
+          <b-row>
+            <b-row class="w-100">
+              <b-col cols="3">
+                <span>Откуда</span>
+              </b-col>
+              <b-col cols="4" class="border-bottom text-center">
+                <span></span>
+              </b-col>
+              <b-col class="text-right">
+                <span>Пункт отправления</span>
+              </b-col>
+            </b-row>
+          </b-row>
+          <b-row>
+            <b-row class="w-100">
+              <b-col cols="3">
+                <span>Куда</span>
+              </b-col>
+              <b-col cols="4" class="border-bottom text-center">
+                <span></span>
+              </b-col>
+              <b-col class="text-right">
+                <span>Пункт прибывания</span>
+              </b-col>
+            </b-row>
+          </b-row>
+          <b-row>
+            <b-row class="w-100">
+              <b-col cols="3">
+                <span>Судно</span>
+              </b-col>
+              <b-col cols="4" class="border-bottom text-center">
+                <span></span>
+              </b-col>
+              <b-col class="text-right">
+                <span>Метеор «Быстрый»</span>
+              </b-col>
+            </b-row>
+          </b-row>
+          <b-row>
+            <b-row class="w-100">
+              <b-col cols="3">
+                <span>Время в пути</span>
+              </b-col>
+              <b-col cols="4" class="border-bottom text-center">
+                <span></span>
+              </b-col>
+              <b-col class="text-right">
+                <span>2 часа</span>
+              </b-col>
+            </b-row>
+          </b-row>
+          <b-row>
+            <b-row class="w-100">
+              <b-col cols="3">
+                <span>Код бронирования</span>
+              </b-col>
+              <b-col cols="4" class="border-bottom text-center">
+                <span></span>
+              </b-col>
+              <b-col class="text-right">
+                <span>GADLVO</span>
+              </b-col>
+            </b-row>
+          </b-row>
+          <b-row>
+            <b-row class="w-100">
+              <b-col cols="3">
+                <span>Взрослый</span>
+              </b-col>
+              <b-col cols="4" class="border-bottom text-center">
+                <span></span>
+              </b-col>
+              <b-col class="text-right">
+                <span><b>1800</b></span>
+              </b-col>
+            </b-row>
+          </b-row>
+          <b-row class="d-flex justify-content-center pt-3">
+            <b-button variant="outline-secondary">Отменить поездку</b-button>
+          </b-row>
+        </b-col>
+        <b-col>
+          <div cols="5" class="border m-4 p-5">
+            <div class="border-bottom d-flex flex-column align-items-center">
+              <div>
+                <img src="../../assets/image1.png" alt="qrCode">
+              </div>
+              <div>
+                <h4><b>№ 647019639200</b></h4>
+              </div>
+            </div>
+            <b-row class="border-bottom text-center">
+              <div class="p-3">
+                <b-row class="text-muted">
+                  <span>Дата отправки</span>
+                </b-row>
+                <b-row>
+                  <span>26 октября 2021 г.</span>
+                </b-row>
+              </div>
+              <b-col></b-col>
+              <div class=" p-3 text-right">
+                <h1><b>14:45</b></h1>
+              </div>
+            </b-row>
+            <b-row>
+              <b-row class="w-100">
+                <b-col cols="3">
+                  <span>Откуда</span>
+                </b-col>
+                <b-col cols="4" class="border-bottom text-center">
+                  <span></span>
+                </b-col>
+                <b-col class="text-right">
+                  <span>Пункт отправления</span>
+                </b-col>
+              </b-row>
+            </b-row>
+            <b-row>
+              <b-row class="w-100">
+                <b-col cols="3">
+                  <span>Куда</span>
+                </b-col>
+                <b-col cols="4" class="border-bottom text-center">
+                  <span></span>
+                </b-col>
+                <b-col class="text-right">
+                  <span>Пункт прибывания</span>
+                </b-col>
+              </b-row>
+            </b-row>
+            <b-row>
+              <b-row class="w-100">
+                <b-col cols="3">
+                  <span>Судно</span>
+                </b-col>
+                <b-col cols="4" class="border-bottom text-center">
+                  <span></span>
+                </b-col>
+                <b-col class="text-right">
+                  <span>Метеор «Быстрый»</span>
+                </b-col>
+              </b-row>
+            </b-row>
+            <b-row>
+              <b-row class="w-100">
+                <b-col cols="3">
+                  <span>Время в пути</span>
+                </b-col>
+                <b-col cols="4" class="border-bottom text-center">
+                  <span></span>
+                </b-col>
+                <b-col class="text-right">
+                  <span>2 часа</span>
+                </b-col>
+              </b-row>
+            </b-row>
+            <b-row>
+              <b-row class="w-100">
+                <b-col cols="3">
+                  <span>Код бронирования</span>
+                </b-col>
+                <b-col cols="4" class="border-bottom text-center">
+                  <span></span>
+                </b-col>
+                <b-col class="text-right">
+                  <span>GADLVO</span>
+                </b-col>
+              </b-row>
+            </b-row>
+            <b-row>
+              <b-row class="w-100">
+                <b-col cols="3">
+                  <span>Взрослый</span>
+                </b-col>
+                <b-col cols="4" class="border-bottom text-center">
+                  <span></span>
+                </b-col>
+                <b-col class="text-right">
+                  <span><b>3600</b></span>
+                </b-col>
+              </b-row>
+            </b-row>
+            <b-row class="d-flex justify-content-center pt-3">
+              <b-button variant="outline-secondary">Отменить поездку</b-button>
+            </b-row>
+          </div>
+        </b-col>
+      </b-container>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'QrCodesPage',
+};
+</script>
+
+<style scoped lang="scss">
+  .greyBackground{
+    background: #F8F9FA;
+  }
+</style>
